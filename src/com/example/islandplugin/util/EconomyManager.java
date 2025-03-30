20e6eaa3e4ffdacf178766380e615f9a172a07a8
sunwoo173 <kimdongmin12355@gmail.com>
2025-03-30 19:10:13 +0900

    Create EconomyManager.java

---
+++ src/com/example/islandplugin/util/EconomyManager.java
@@ -0,0 +1,14 @@
+package com.example.islandplugin.util;
+
+public class EconomyManager {
+
+    // 가상의 경제 시스템
+    public boolean hasEnoughMoney(Player player, int amount) {
+        // 실제 경제 시스템이 있는 경우 확인할 수 있는 로직 작성
+        return true; // 예시로 무조건 true 리턴
+    }
+
+    public void deductMoney(Player player, int amount) {
+        // 실제 경제 시스템에 맞게 돈 차감하는 로직 작성
+    }
+}
